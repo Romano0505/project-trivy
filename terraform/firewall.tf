98c2c493e53ff381e0f249730ff379a55b424a7d
DanielleCasey <potgieterdanielle989@gmail.com>
2026-06-12 07:59:47 +0200

    feat: update network, compute, firewall, and iam configs

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -1,11 +1,13 @@
 resource "google_compute_firewall" "allow_ssh" {
-  name    = "allow-ssh"
+  name    = "datastream-allow-ssh"
   network = google_compute_network.vpc.id
+
+  description = "Allow SSH access to VM for management"
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
 
-  source_ranges = ["0.0.0.0/0"] # tighten later if needed
+  source_ranges = ["0.0.0.0/0"]
 }
