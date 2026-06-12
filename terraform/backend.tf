559ab08b91b1d44cde1d50ed44f9aefc34e64971
DanielleCasey <potgieterdanielle989@gmail.com>
2026-06-12 10:03:24 +0200

    added name of bucket and other file code

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "datastream2-tf-state" # Change this to your unique bucket name
+    bucket = "project-repo-498812-terraform-state" # Change this to your unique bucket name
     prefix = "terraform/state"
   }
 } 
