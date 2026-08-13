ecaf43e74540f1aad1343014e224548f16b2decd
OmTalent 54 <omtalent54@OmTalents-MacBook-Air.local>
2026-08-13 10:41:21 +0200

    nmap caller

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
-output "airflow_vm_name" {
-  value       = google_compute_instance.airflow_vm.name
-  description = "The name of the Airflow VM instance"
+output "free_vm_name" {
+  value       = google_compute_instance.free_vm.name
+  description = "The name of the free tier VM instance"
 }
 
-output "airflow_vm_private_ip" {
-  value       = google_compute_instance.airflow_vm.network_interface[0].network_ip
-  description = "The internal IP address of the Airflow VM"
+output "free_vm_private_ip" {
+  value       = google_compute_instance.free_vm.network_interface[0].network_ip
+  description = "The internal IP address of the free tier VM"
 }
 
 output "vpc_network_name" {
